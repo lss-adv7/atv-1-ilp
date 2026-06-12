749d1aa47f750d4edfc5ec17e54f4abb0760a5cd
lss-adv7 <lailsonsantana08@gmail.com>
2026-06-11 23:29:57 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,12 @@
+
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    soma = a + b
+    return soma
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
